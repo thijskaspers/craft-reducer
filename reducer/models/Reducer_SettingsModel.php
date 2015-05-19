aa2242ca41adf4877dd078b223422e05519e1da5
Thijs Kaspers <thijs@24creative.nl>
2015-05-19 15:18:56 +0200

    Minimum size and quality values set to 1

--- reducer/models/Reducer_SettingsModel.php
+++ reducer/models/Reducer_SettingsModel.php
@@ -8,8 +8,8 @@ class Reducer_SettingsModel extends BaseModel
 		return array(
 			'id'  => AttributeType::Number,
 			'source'  => array(AttributeType::Number, 'required' => true),
-			'maxSize'  => array(AttributeType::Number, 'label' => Craft::t('Max. size'), 'required' => true, 'min' => 0),
-			'quality'  => array(AttributeType::Number, 'label' => Craft::t('Quality'), 'min' => 0, 'max' => 100),
+			'maxSize'  => array(AttributeType::Number, 'label' => Craft::t('Max. size'), 'required' => true, 'min' => 1),
+			'quality'  => array(AttributeType::Number, 'label' => Craft::t('Quality'), 'min' => 1, 'max' => 100),
 			'enabled'  => array(AttributeType::Bool, 'label' => 'Enabled'),
 		);
 	}
