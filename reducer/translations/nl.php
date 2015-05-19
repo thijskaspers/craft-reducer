e8d325bb201e1700811ac8e3b150726bef2feec8
Thijs Kaspers <thijs@24creative.nl>
2015-05-19 10:59:37 +0200

    Dutch translations

--- reducer/translations/nl.php
+++ reducer/translations/nl.php
@@ -4,4 +4,8 @@ return array(
 	'Asset source' => 'Bestandsbron',
 	'Max. size' => 'Max. grootte',
 	'Settings saved' => 'Instellingen opgeslagen.',
+	'Source settings' => 'Bestandsbron instellingen',
+	'Maximum size in pixels. Larger images will be downsized to fit within this value.' => 'Maximale grootte in pixels. Grotere afbeeldingen worden verkleind om binnen deze waarde te passen.',
+	'Quality in percentage.' => 'Kwaliteit in procenten.',
+	'(Optional)' => '(Optioneel)',
 );
